18f0600ad79e2c47106d4b8f73272aad8ba8f170
Mrr Hak <longkimhak.kh@gmail.com>
2025-06-21 17:03:46 +0700

    style: improve code formatting and indentation consistency

--- lib/src/model/deeplink_info/deeplink_info.g.dart
+++ lib/src/model/deeplink_info/deeplink_info.g.dart
@@ -10,9 +10,10 @@ _DeeplinkInfo _$DeeplinkInfoFromJson(Map<String, dynamic> json) =>
     _DeeplinkInfo(
       qr: json['qr'] as String,
       url: json['url'] as String,
-      sourceInfo: json['sourceInfo'] == null
-          ? null
-          : SourceInfo.fromJson(json['sourceInfo'] as Map<String, dynamic>),
+      sourceInfo:
+          json['sourceInfo'] == null
+              ? null
+              : SourceInfo.fromJson(json['sourceInfo'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$DeeplinkInfoToJson(_DeeplinkInfo instance) =>
